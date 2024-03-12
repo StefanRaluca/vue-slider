8936932f915997525d929fa46db96c7ed04e5995
StefanRaluca <raluca.bubulina@yahoo.com>
2024-03-12 15:42:28 +0100

    add app js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,28 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-        
-        }
+     //facciamo il array per le img
+            images: [
+                { src: "./assets/img/01.webp" },
+                { src: "./assets/img/02.webp" },
+                { src: "./assets/img/03.webp" },
+                { src: "./assets/img/04.webp" },
+                { src: "./assets/img/05.webp" }
+            ],
+            // partiamo nel array con la posizione 0
+       activeImg: 0,
+        };
+    },
+    //selezioniamo i btn tramite i methods 
+    methods: {
+        upBtn() {
+            this.activeImg = (this.activeImg + 1) % this.images.length;
 
+        },
+        downBtn() {
+            this.activeImg = (this.activeImg - 1 + this.images.length) % this.images.length;
+
+        }
 
     }
 
